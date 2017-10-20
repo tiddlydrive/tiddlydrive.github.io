97db6596fcc8ded4eb6208f75c5adf79fad2d40e
Joshua <joshuastubbs6@gmail.com>
2017-10-20 07:20:50 +0200

    Initialise modal

--- index.js
+++ index.js
@@ -125,4 +125,8 @@
     }
   }
   setupSaver();
+  $(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
 })();
