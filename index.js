89f3416793c2c5208a58019aee29801a70df7ad2
Joshua <joshuastubbs6@gmail.com>
2017-10-20 07:40:01 +0200

    Store autosave

--- index.js
+++ index.js
@@ -49,11 +49,27 @@
     }
   }
 
-  $('#auth').click(function(){
-    if(!gapi.auth2.getAuthInstance().isSignedIn.get()) {
-      gapi.auth2.getAuthInstance().signIn();
-    }
-  });
+
+  function createCookie(name,value,days) {
+      var expires = "";
+      if (days) {
+          var date = new Date();
+          date.setTime(date.getTime() + (days*24*60*60*1000));
+          expires = "; expires=" + date.toUTCString();
+      }
+      document.cookie = name + "=" + value + expires + "; path=/";
+  }
+
+  function readCookie(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(';');
+      for(var i=0;i < ca.length;i++) {
+          var c = ca[i];
+          while (c.charAt(0)==' ') c = c.substring(1,c.length);
+          if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      }
+      return null;
+  }
 
   function getParameterByName(name, url) {
       if (!url) url = window.location.href;
@@ -126,7 +142,18 @@
   }
   setupSaver();
   $(document).ready(function(){
-    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    $('#hide-fab').click(function(){
+      $('#open-settings').hide();
+    });
+    $('#auth').click(function() {
+      if(!gapi.auth2.getAuthInstance().isSignedIn.get()) {
+        gapi.auth2.getAuthInstance().signIn();
+      }
+    });
+    $('enable-autosave').val(readCookie(readCookie('enable-autosave') !=== 'false');
+    $('enable-autosave').change(function() {
+      createCookie('enable-autosave', this.checked, 364);
+    })
   });
 })();
